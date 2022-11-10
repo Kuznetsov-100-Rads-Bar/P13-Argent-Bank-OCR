ce84dc0c698cb39c2ba65005a71d208949a5ad70
Kuznetsov-100-Rads-Bar <ledrifterdepripyat@gmail.com>
2022-11-10 01:11:24 +0100

    Ajout de l'extension redux devtool

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,4 @@
-import { applyMiddleware, combineReducers, legacy_createStore as createStore } from 'redux'
+import { applyMiddleware, combineReducers, compose, legacy_createStore as createStore } from 'redux'
 import { userDataMiddleware } from "./middlewares/UserData.middlewares";
 import UserDataReducers from "./reducers/UserData.reducers";
 
@@ -6,8 +6,10 @@ const middlewares = applyMiddleware(
   userDataMiddleware
 );
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const reducers = combineReducers({
   userData: UserDataReducers
 });
 
-export const store = createStore(reducers, middlewares /*&& window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()*/)
+export const store = createStore(reducers, composeEnhancers(middlewares))
